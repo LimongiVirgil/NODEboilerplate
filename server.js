1da6f8134c9815bd38c645a5cba9a9ab84d2628b
LimongiVirgil <limongi.virgil@gmail.com>
2019-05-15 15:16:01 +0200

    add env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*
 Importer les composants serveur
 */
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -9,7 +10,7 @@ Configuration du serveur
 */
 // Définir les variables serveur
 const server = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Définition du dossier static du client
 server.set( 'views', __dirname + '/www' );
